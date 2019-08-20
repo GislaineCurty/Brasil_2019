fd6c091f53b60d2e76769d89ab19340af3ea910f
GislaineCurty <45762793+GislaineCurty@users.noreply.github.com>
2019-08-20 16:17:21 -0300

    Update 6.R

--- Assigment-4/6.R
+++ Assigment-4/6.R
@@ -5,4 +5,4 @@ times=3
 
 
 PredictPopulationGrowth=function(bactname, numberbact0, growthRateDay)return(paste("After 3 days, there will be", numberbact0*growthRateDay*times,"of",bactname))
-print(PredictPopulationGrowth(bactname, 1000,growthRateDay))
+print(PredictPopulationGrowth(bactname,numberbact0,growthRateDay))
